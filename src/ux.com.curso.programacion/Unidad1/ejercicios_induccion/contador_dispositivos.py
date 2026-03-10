582b700272b5095b83ffe4796485c45bb97b3987
Jesus Luna <jes24.lu@gmail.com>
2026-03-10 07:31:08 -0600

    contador dispositivos

--- src/ux.com.curso.programacion/Unidad1/ejercicios_induccion/contador_dispositivos.py
+++ src/ux.com.curso.programacion/Unidad1/ejercicios_induccion/contador_dispositivos.py
@@ -1,5 +1,7 @@
 # Desarrollo de algoritmo contador de dispositivos
-
+"""
+Definicion de la funcion para el control del contador
+"""
 def contador_positivos():
     contador = 0
     while True:
@@ -10,6 +12,9 @@ def contador_positivos():
     print("cantidad de numeros positivos ingresados: ", contador)
     
 # Definicion de la funcion main
+"""
+Definicion de la funcion principal del programa para iniciar el contador de dispositivos
+"""
 def main():
     print("Bienvenido al contador de dispositivos")
     contador_positivos()

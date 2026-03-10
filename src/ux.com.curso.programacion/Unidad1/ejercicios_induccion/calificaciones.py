a99ef826ff4c121ea48505afbab48f2875bac8ff
Jesus Luna <jes24.lu@gmail.com>
2026-03-10 08:56:34 -0600

    calificaciones

--- src/ux.com.curso.programacion/Unidad1/ejercicios_induccion/calificaciones.py
+++ src/ux.com.curso.programacion/Unidad1/ejercicios_induccion/calificaciones.py
@@ -1,6 +1,13 @@
 # Programa para determinar califaciones en USA
+"""
+Se define la funcion para determinar las calificaciones
+"""
 def calificaciones_usa():
     calificacion = float(input("Ingrese su calificación (0-100): "))
+    
+    """ 
+    Se establecen las condiciones para determinar la calificación
+    """
     
     if calificacion >= 90:
         print("Su calificación es: A")
@@ -13,8 +20,14 @@ def calificaciones_usa():
     else:
         print("Su calificación es: F")
 
+# Definicion de la funcion main para iniciar el programa
+"""
+Definicion de la funcion principal del programa para iniciar el contador de dispositivos
+"""
 def main():
     calificaciones_usa()
+    
+# Llamada a la funcion main para iniciar el programa
 
 if __name__ == "__main__":
     main()

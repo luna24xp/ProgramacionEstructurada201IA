e508a010962aa3972fe9908c97c3b3d21eaa3679
Jesus Luna <jes24.lu@gmail.com>
2026-03-13 08:17:40 -0600

    ejercicio cadenas

---
+++ src/ux.com.curso.programacion/Unidad1/ejercicios_induccion/Manejo_cadenas/ejercicio_cadenas.py
@@ -0,0 +1,15 @@
+#EJERCICIO DE CADENAS
+
+def demostracion_cadenas(cadena):
+    cadena = "universidad de xalapa"
+
+    print (cadena.upper()) 
+    print (cadena.lower())
+    print (cadena.capitalize())
+    print (cadena.title())
+    
+def main():
+    demostracion_cadenas(cadena = "universidad de xalapa")
+
+if __name__ == "__main__":
+    main()

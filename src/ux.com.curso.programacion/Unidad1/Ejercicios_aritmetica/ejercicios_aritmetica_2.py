cdab2fcf5dd1a5bd8f0fb701a048a2b5f14c9620
Jesus Luna <jes24.lu@gmail.com>
2026-03-17 07:34:55 -0600

    ejercicios aritmetica 2

---
+++ src/ux.com.curso.programacion/Unidad1/Ejercicios_aritmetica/ejercicios_aritmetica_2.py
@@ -0,0 +1,23 @@
+import math
+
+# demostracion del uso de funciones de math
+
+def mostrar_funciones_math():
+    #crear una variable
+    numero = 20
+    
+    sen_x = math.sin(numero)
+    cos_x = math.cos(numero)
+    
+    print(f"El seno de {numero} es: {sen_x}")
+    print(f"El coseno de {numero} es: {cos_x}")
+
+    resultado = sen_x ** 2 + cos_x ** 2
+    print(f"El resultado de sen^2 + cos^2 es: {resultado}")
+    
+def main():
+    mostrar_funciones_math()
+    
+if __name__ == "__main__":
+    main()
+    

2d8d60b6e99f0ce7352612ecc7f06cf29a01bf98
Jesus Luna <jes24.lu@gmail.com>
2026-03-23 07:49:03 -0600

    simulacion_entrenaimeiento_lotes

---
+++ src/ux.com.curso.programacion/Unidad1/Examen_p1/Simulacion_entrenamiento_lotes.py
@@ -0,0 +1,29 @@
+# Ejemplo de una simulación de entrenamiento por lotes
+
+"""
+Se define la función
+"""
+def simulacion_entrenamiento_lotes():
+    tam_lotes = int(input("Ingrese el tamaño del lote de tensores: "))
+    
+    """
+    Se establece la condición para prevenir el error "out of memory"
+    """
+    if tam_lotes >= 2500:
+        print(f"El tamaño del lote de tensores es: {tam_lotes}")
+        print("El proceso de carga de ha detenido debido a que el tamaño del lote es demasiado grande.")
+    else:
+        print(f"El tamaño del lote de tensores es: {tam_lotes}")
+        print("El proceso de entrenamiento se ha iniciado correctamente.")
+
+# Definición de la función main para iniciar el programa
+"""
+Definicion de la funcion principal del programa
+"""
+def main():
+    simulacion_entrenamiento_lotes()
+    
+# Llamada a la funcion main para iniciar el programa
+
+if __name__ == "__main__":
+    main()

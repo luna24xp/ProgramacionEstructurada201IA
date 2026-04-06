c9645818a0484674cfd681c7c339a3d44aeb6ca7
Jesus Luna <jes24.lu@gmail.com>
2026-04-06 08:23:36 -0600

    Ejercicio 3

--- src/ux.com.curso.programacion/unidad2/mermaid/ejercicio03.py
+++ src/ux.com.curso.programacion/unidad2/mermaid/ejercicio03.py
@@ -1,11 +1,16 @@
 #Diseñar algoritmo para generar una serie de números impares que generan una lista secuencial de los primeros N números impares (comenzando desde el 1).
 
-N  = int(input("Ingrese el número de elementos de la serie de números impares: "))
+def contador_impares(N):
+    contador = 0
+    numero = 1
+    while contador < N:
+        print(numero)
+        numero += 2
+        contador += 1
+    
+def main():
+    N = int(input("Ingrese un numero: "))
+    contador_impares(N)
 
-contador = 0
-numero= 1
-
-while contador < N:
-    print(numero)
-    numero += 2
-    contador += 1
+if __name__ == "__main__":
+    main()

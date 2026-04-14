b105e2ba162978b391026e8cbdb28312e9e78f03
Jesus Luna <jes24.lu@gmail.com>
2026-04-14 07:48:37 -0600

    creacion de archivo carga de data y funcion limpieza v2

--- src/ux.com.curso.programacion/unidad3/practica_02/Analizador_Predictivo.py
+++ src/ux.com.curso.programacion/unidad3/practica_02/Analizador_Predictivo.py
@@ -23,6 +23,12 @@ def calcular_alerta(valor_normalizado):
 	y 'NORMAL' en cualquier otro caso.
 	"""
 # IMPLEMENTAR AQUÍ
+	if valor_normalizado > 0.8:
+		return 'CRÍTICO'
+	elif valor_normalizado > 0.5:
+		return 'PRECAUCIÓN'
+	else:
+		return 'NORMAL'
 pass
 def obtener_estadisticas(lista_datos):
 	"""
